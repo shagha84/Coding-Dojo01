000156a940b1e1df92747f84c6c4835ab8485f05
Shaghayegh Yaghtin <wi19b085@technikum-wien.at>
2020-10-04 14:37:48 +0200

    Projektdateien hinzufügen.

---
+++ Dojo01/Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dojo01
+{
+    class Stack<T>
+    {
+        private StackElement<T> _currentElement; //stores the latest entry of the stack
+
+        /// <summary>
+        /// Adds new Elements to the stack 
+        /// </summary>
+        /// <param name="item">item which should be added to the stack</param>
+        public void Push(T item)
+        {
+            if (_currentElement == null)
+            {
+                _currentElement = new StackElement<T>() { ValueOfElement = item, Successor = null };
+            }
+            else
+            {
+                StackElement<T> temp = new StackElement<T>() { ValueOfElement = item, Successor = _currentElement };
+                _currentElement = temp;
+            }
+
+        }
+
+        /// <summary>
+        /// Removes the last entry from the Stack
+        /// If the stack is empty pop will return a default value
+        /// </summary>
+        /// <returns>deleted stack entry</returns>
+        public T Pop()
+        {
+            if (_currentElement != null)
+            {
+                T temp = _currentElement.ValueOfElement;
+                _currentElement = _currentElement.Successor;
+                return temp;
+            }
+            else
+            {
+                throw new NullReferenceException(); //throw an exception because stack is entry
+            }
+        }
+
+        /// <summary>
+        /// Returns the value of the last entry (on top of the stack)
+        /// If the stack is empty pop will return a default value
+        /// </summary>
+        /// <returns>value of current stack entry</returns>
+        public T Peek()
+        {
+            if (_currentElement != null)
+            {
+                return _currentElement.ValueOfElement;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+    }
+}
---
+++ Dojo01/StackElement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dojo01
+{
+    class StackElement<T>
+    {
+        public T ValueOfElement { get; set; } //Stores the value of the stack entry
+        public StackElement<T> Successor { get; set; } //points to the successor of this entry => the entry which was added before this one
+    }
+}
